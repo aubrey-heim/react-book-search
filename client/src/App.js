8a12ba15e2902ad837d96211fa59398c43619e9c
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-14 11:30:52 -0800

    header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,71 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Saved from "./pages/Saved"
+import Search from "./pages/Search"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  AppBar, 
+  Toolbar,
+  Typography,
+  MenuItem,
+  IconButton,
+}from '@material-ui/core/';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import { makeStyles } from '@material-ui/core/styles';
+
+const classes = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    textAlign: "left"
+  },
+  buttons: {
+    marginRight:"5px",
+    height: "40px",
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
   return (
     <div className="App">
-      
+      <AppBar position="static">
+        <Toolbar>
+          <Typography edge="start" variant="h6" className={classes.title}>
+            Google Books Search
+          </Typography>
+          <MenuItem style={{marginLeft:"10px"}}>
+            <IconButton 
+                href="/search"
+                color="inherit"
+                style={{fontSize:"12px"}}>
+                    <PageviewIcon /> Search
+            </IconButton>
+          </MenuItem>
+          <MenuItem>
+            <IconButton 
+                href="/saved"
+                color="inherit"
+                style={{fontSize:"12px"}}>
+                    <BookmarksIcon /> Saved
+            </IconButton>
+          </MenuItem>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Switch>
+          <Route exact path={["/", "/search"]}>
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
